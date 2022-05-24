5117ed63d84ebb4ac75a909030e4b6a937029abd
Kris Rodriguez <k.rod.518@gmail.com>
2022-05-24 07:39:43 -0500

    Add Round Page
    Add Round page added w/out functionality, also changed navbar a bit

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,20 +1,59 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
+import { Collapse } from "bootstrap";
+import logo from "../img/logo.png";
+import banner from "../img/banner_main.jpg"
 
 const TopNav = () => {
-    return (
-        <div className="bg-white pt-4 container">
-            <div className="d-flex justify-content-between pb-5">
-                <div className="">
-                    <img src={require("../img/logo.jpg")} style={{width:"30%"}} alt="Tee-Time Logo" />
-                </div>
-                <div className="d-flex gap-3 justify-content-end fs-6 mt-3">
-                    <p className=" fw-bold">Dashboard</p>
-                    <p className=" fw-bold">Add Round</p>
-                    <p className=" fw-bold">Team</p>
-                </div>
-            </div>
+  const [toggle, setToggle] = useState(false);
+  const collapseRef = useRef();
+
+  const navBarCollapse = () => {
+    let myCollapse = collapseRef.current;
+    let bsCollapse = new Collapse(myCollapse, { toggle: false });
+    toggle ? bsCollapse.show() : bsCollapse.hide();
+    setToggle((toggle) => !toggle);
+  };
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <div className="d-flex navbar-brand">
+            <img className="w-50 mt-1 me-1" src={logo} alt="tee-time logo" />
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+            onClick={navBarCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navmenu">
+            <ul className="navbar-nav ms-auto">
+              <li className="mx-2 nav-item">
+                <Link className="text-decoration-none nav-link" to="/">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="mx-2 nav-item">
+                <Link className="text-decoration-none nav-link" to="/addround">
+                  Add Round
+                </Link>
+              </li>
+              <li className="mx-2 nav-item">
+                <Link className="text-decoration-none nav-link" to="/login">
+                  Log Out
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
-}
+      </nav>
+    </>
+  );
+};
 
 export default TopNav;
